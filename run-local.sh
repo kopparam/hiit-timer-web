f985301f52fe27b88c909dfee2ac922e9008404f
Kashyap Kopparam <kashyapkopparam@gmail.com>
2020-10-04 16:47:16 +0800

    Working helm deployment to local miniqube

--- run-local.sh
+++ run-local.sh
@@ -8,7 +8,10 @@ helm upgrade --install appdb bitnami/postgresql \
   --namespace springfun \
   --wait --timeout 1m
 
+export POSTGRES_PASSWORD=$(kubectl get secret --namespace springfun appdb-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
+
 helm upgrade --install webapp ./helm/springfun \
   --namespace springfun \
   --values ./helm/local.yaml \
+  --set env.SPRING_DATASOURCE_PASSWORD=$POSTGRES_PASSWORD \
   --wait --timeout 1m
